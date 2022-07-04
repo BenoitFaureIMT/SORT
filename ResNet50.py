6ac082060c461a0f1e3ea486c66ca390721613e5
NathanOD <nathan.odic@mines-ales.org>
2022-07-04 12:27:49 +0200

    resnet

--- ResNet50.py
+++ ResNet50.py
@@ -1,28 +1,23 @@
 import tensorflow as tf
 from tensorflow.python.keras import layers
+import cv2
+import numpy as np
 
-#tf.keras.datasets.cifar10.load_data()
-
-def preprocess(X, Y):
-    X1 = tf.keras.applications.resnet50.preprocess_input(X)
-    Y1 = tf.keras.utils.to_categorical(Y,10)
-    return X1, Y1
-
-(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
-x_train, y_train = preprocess(x_train, y_train)
-x_test, y_test = preprocess(x_test, y_test)
-
-input = tf.keras.Input(shape=(32, 32, 3))
-model = tf.keras.applications.resnet50.ResNet50(
-    include_top=True,
-    weights='imagenet',
+model = tf.keras.applications.ResNet50V2(
+    include_top=False,
+    weights="imagenet",
     input_tensor=None,
-    input_shape=None,
+    input_shape=(64,64,3),
     pooling=None,
     classes=1000,
+    classifier_activation="softmax",
 )
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-poids = tf.keras.callbacks.ModelCheckpoint(filepath="poids.h5", monitor="val_acc", mode="max", save_best_only=True)
-truc = model.fit(x_train, y_train, batch_size=32, epochs=10, validation_data=(x_test, y_test), callbacks=[poids])
 model.summary()
-model.save("poids.h5")
+
+image = cv2.imread("Elephant10.jpg")
+image = cv2.resize(image, (64, 64), cv2.INTER_LINEAR)/255
+image = np.array([image])
+image = tf.keras.applications.resnet50.preprocess_input(image)
+result = model.predict(image)
+#print(result[-1,-1])
+print(result.shape)
