230bc1e38c31c1876b1a0def85f4374d0fd77956
NathanOD <nathan.odic@mines-ales.org>
2022-07-05 15:30:10 +0200

    French Grils

--- association.py
+++ association.py
@@ -1,5 +1,6 @@
 # https://github.com/nwojke/deep_sort/blob/master/deep_sort/linear_assignment.py
 import numpy as np
+from scipy.optimize import linear_sum_assignment
 
 track_indices = [] # 1 - ... - N
 detect_indices = [] # 1 - ... - M
@@ -11,6 +12,8 @@ mat_cov = np.identity(7)
 pred_kal = [] #xywh liste des tracks prédits
 bbox = [] #xywh liste des détections
 min_cosine_dist = np.zeros((len(track_indices),len(detect_indices))) # Matrice N x M
+tracks = []
+detections = []
 
 def d1(i,j):
     mat = bbox[:][j]-pred_kal[i]
@@ -38,21 +41,52 @@ def c(i,j,lmbd):
     return lmbd*d1(i,j)+(1-lmbd)*d2(i,j)
 
 
-# Step 1
-cost_matrix = [[c(i,j) for i in range(len(track_indices))] for j in range(len(detect_indices))]
-# Step 2
+
+
+
+def min_cost_matching(func_dist_metric, track, detection, T, U, max_dist=0.9):
+    if len(U) == 0 or len(T) == 0:
+        return [], T, U
+    cost_matrix = func_dist_metric(c, T, U)
+    cost_matrix[cost_matrix > max_dist] = max_dist + 1e-5 # Coeff a potentiellement changer
+    row_indices, col_indices = linear_sum_assignment(cost_matrix)
+    
+    matches, unmatched_tracks, unmatched_detections = [], [], []
+    for col, detection_idx in enumerate(U):
+        if col not in col_indices:
+            unmatched_detections.append(detection_idx)
+    for row, track_idx in enumerate(track_indices):
+        if row not in row_indices:
+            unmatched_tracks.append(track_idx)
+    for row, col in zip(row_indices, col_indices):
+        track_idx = track_indices[row]
+        detection_idx = U[col]
+        if cost_matrix[row, col] > max_dist:
+            unmatched_tracks.append(track_idx)
+            unmatched_detections.append(detection_idx)
+        else:
+            matches.append((track_idx, detection_idx))
+    return matches, unmatched_tracks, unmatched_detections
+    
+
+
+
+
+
+def dist_metric(func_c, track_ind, detect_ind):
+    return [[func_c(i,j) for i in range(len(track_ind))] for j in range(len(detect_ind))]
+
+# Matching Cascade
+#cost_matrix = [[c(i,j) for i in range(len(track_indices))] for j in range(len(detect_indices))]
 gate_matrix = [[b(i,j) for i in range(len(track_indices))] for j in range(len(detect_indices))]
-# Step 3
 matches = []
-# Step 4
 unmatched = detect_indices
-# Step 5
 for i in range(1,max_age):
     Tn = []
     for j in range(len(track_indices)):
         if j>0: # Changer condition : Tous les tracks non sélectionnés lors des n frames précédentes
             Tn.append(track_indices[j])
-    X = min_cost_matching(cost_matrix,Tn,unmatched)
+    X = min_cost_matching(dist_metric, tracks, detections, Tn,unmatched, 0.9)[0]
     for k in range(len(track_indices)):
         for l in range(len(detect_indices)):
             if gate_matrix[i][j]*X[i][j] > 0:
